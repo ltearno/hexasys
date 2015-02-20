8fcff33c55a029cac2a4c600903228e019bb4c7b
Arnaud Tournier <arnaud.tournier@rigpa.org>
2015-02-20 13:22:18 +0100

    Added BC method emulation for ganapati

--- tools/utils.inc.php
+++ tools/utils.inc.php
@@ -1,5 +1,26 @@
 <?php
 
+function bcadd( $a, $b )
+{
+	return round( ($a * 100 + $b * 100) / 100, 2 );
+}
+
+function bcsub( $a, $b )
+{
+	return round( ($a * 100 - $b * 100) / 100, 2 );
+}
+
+function bcmul( $a, $b )
+{
+	return round( $a * $b, 2 );
+}
+
+function bccomp( $a, $b )
+{
+	return $a > $b ? 1 : ($a==$b ? 0 : -1);
+}
+
+
 /** This function converts a String representation of a decimal number
  * comming from a database through PDO.
  * The output value is an integer which is 100 times the value received
