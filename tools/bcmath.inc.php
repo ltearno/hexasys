8fca40ffd3b345b9ed4bc9f3f6ee5eaa061b5812
Arnaud Tournier <ltearno@gmail.com>
2015-02-27 14:19:17 +0100

    BCMath functions are emulated if necessary

---
+++ tools/bcmath.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Emulation of the BCMath library, if not available
+ */
+
+function bcadd( $a, $b )
+{
+	return round( ($a * 100 + $b * 100) / 100, 2 );
+}
+
+function bcsub( $a, $b )
+{
+	return round( ($a * 100 - $b * 100) / 100, 2 );
+}
+
+function bcmul( $a, $b )
+{
+	return round( $a * $b, 2 );
+}
+
+function bccomp( $a, $b )
+{
+	return $a > $b ? 1 : ($a==$b ? 0 : -1);
+}
+
+?>
--- tools/utils.inc.php
+++ tools/utils.inc.php
@@ -1,23 +1,8 @@
 <?php
 
-function bcadd( $a, $b )
+if( ! function_exists('bcadd') )
 {
-	return round( ($a * 100 + $b * 100) / 100, 2 );
-}
-
-function bcsub( $a, $b )
-{
-	return round( ($a * 100 - $b * 100) / 100, 2 );
-}
-
-function bcmul( $a, $b )
-{
-	return round( $a * $b, 2 );
-}
-
-function bccomp( $a, $b )
-{
-	return $a > $b ? 1 : ($a==$b ? 0 : -1);
+	include_once 'bcmath.inc.php';
 }
 
 
