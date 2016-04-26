31bc9121047490ceec8da1dcfb72bfe60287633a
Arnaud Tournier <ltearno@gmail.com>
2016-04-26 17:21:13 +0200

    documentation

--- lib/BkgndJobs.inc.php
+++ lib/BkgndJobs.inc.php
@@ -4,25 +4,45 @@
 */
 
 
-// interface that is needed to be implemented by the JobMngs classes
+/**
+ * Interface IBkgndJobMng
+ *
+ * Interface of Jobs which are ran periodically in the background, typically through the cron daemon
+ */
 interface IBkgndJobMng
 {
     public function Init( $db, $qpath );
 
-    // When a job is first created, this method is called for the JobMng to install its data.
-    // This method must return a value that will used to call the JobMng back when stepping the Job
+    /**
+     * When a job is first created, this method is called for the JobMng to install its data.
+     * This method must return a value that will used to call the JobMng back when stepping the Job
+     *
+     * @param $parameters
+     */
     public function OnCreate( $parameters );
 
-    // The $ctx param is the value of the last stored Job state variable.
-    // If the job is finished, the $finished var must be set to 1
-    // The method can modify the $ctx variable, and it will be stored accross calls for the next Step() call
-    public function Step( &$ctx, &$finished );
+    /**
+     * Main invocation method for the job. This is called periodically by the system.
+     *
+     * @param $ctx          value of the last stored Job state variable. Implementation can modify the $ctx variable, and it will be stored accross calls for the next Step() call
+     * @param $finished     If the job is finished, the $finished var must be set to 1
+     * @param $nextCallTime date-time at which the job wants to be called again
+     */
+    public function Step( &$ctx, &$finished, &$nextCallTime );
 
-    // called when the job is destroyed, it is the moment for the JobMng to free its resources, if any
+    /**
+     * Called when the job is destroyed, it is the moment for the JobMng to free its resources, if any
+     *
+     * @param $ctx job context
+     */
     public function OnDelete( $ctx );
 
-    // Provided the current $ctx value of the job, this method should return a string shortly describing
-    // the job state
+    /**
+     * Provided the current $ctx value of the job, this method should return a string shortly describing
+     * the job state
+     *
+     * @param $ctx job context
+     */
     public function GetDescription( $ctx );
 }
 
@@ -38,6 +58,8 @@ class BkgndJobs extends HexaComponentImpl
         include_once($classContainer);
 
         $classToCall = "job_" . $jobAddress;
+
+        /** @var IBkgndJobMng $jobMng */
         $jobMng = new $classToCall();
 
         if( $jobMng != null )
@@ -82,23 +104,32 @@ class BkgndJobs extends HexaComponentImpl
         if( $job == null )
             return -1;
 
+        echo "Execute job " . $job["bkgnd_jobs.id"] . " " . $job["bkgnd_jobs.job_mng_address"] . "<br/>";
+        ob_flush();
+
         $jobMng = $this->getJobMngInstance( $job['bkgnd_jobs.job_mng_address'] );
         if( $jobMng == null )
             return -2;
 
         $ctxVar = HLibStoredVariables()->Read( SYS_JOBS, $job['bkgnd_jobs.ctx_variable_name'] );
 
+        $nextCallTime = $job["bkgnd_jobs.next_call_time"];
+
         echo "BkgndJob context variable dump:<br/>";
         Dump( $ctxVar );
 
         $finished = false;
         $m = HLibMeasure()->Start();
-        $jobMng->Step( $ctxVar, $finished );
+        $jobMng->Step( $ctxVar, $finished, $nextCallTime );
         $ms = HLibMeasure()->End( $m );
 
         HLibStoredVariables()->Store( SYS_JOBS, $job['bkgnd_jobs.ctx_variable_name'], $ctxVar );
 
-        $this->QPath->Update( "bkgnd_jobs", "id=$jobId", array( "exec_time" => $ms + $job['bkgnd_jobs.exec_time'], "times_stepped" => 1 + $job['bkgnd_jobs.times_stepped'], "finished" => ($finished ? 1 : 0) ) );
+        $this->QPath->Update( "bkgnd_jobs", "id=$jobId", array(
+            "next_call_time" => $nextCallTime,
+            "exec_time" => $ms + $job['bkgnd_jobs.exec_time'],
+            "times_stepped" => 1 + $job['bkgnd_jobs.times_stepped'],
+            "finished" => ($finished ? 1 : 0) ) );
 
         return 0;
     }
@@ -129,20 +160,13 @@ class BkgndJobs extends HexaComponentImpl
     // Chooses a non-finished job and step it
     public function Auto()
     {
-        $toStepJobs = $this->QPath->QueryValueList( "bkgnd_jobs [finished=0]", "bkgnd_jobs.id" );
+        $toStepJobs = $this->QPath->QueryValueList( "bkgnd_jobs [finished=0] [next_call_time<=now()]", "bkgnd_jobs.id" );
         if( count( $toStepJobs ) == 0 )
             return; // there is no job to execute
 
         // take one randomly
         shuffle( $toStepJobs );
         $jobId = $toStepJobs[0];
-
-        //$toStepJob = $this->QPath->QueryOne( "O[bkgnd_jobs.times_stepped] bkgnd_jobs [finished=0]" );
-        //if( $toStepJob == null )
-        //	return; // there is no job to execute
-        //$jobId = $toStepJob['bkgnd_jobs.id']
-
-        echo "Execute job " . $jobId . "<br/>";
 
         $this->Step( $jobId );
     }
